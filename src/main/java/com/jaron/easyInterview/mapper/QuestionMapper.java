17f5ea70704323062c144a55fa8e6bd3ea448411
jaron <13617035236@163.com>
2024-10-12 10:04:24 +0800

    es全量和增量同步

--- src/main/java/com/jaron/easyInterview/mapper/QuestionMapper.java
+++ src/main/java/com/jaron/easyInterview/mapper/QuestionMapper.java
@@ -2,6 +2,10 @@ package com.jaron.easyInterview.mapper;
 
 import com.jaron.easyInterview.model.entity.Question;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Date;
+import java.util.List;
 
 /**
 * @author jaron
@@ -10,6 +14,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.jaron.easyInterview.model.entity.Question
 */
 public interface QuestionMapper extends BaseMapper<Question> {
+
+    /**
+     * 查询题目列表（包括已被删除的数据）
+     */
+    @Select("select * from question where updateTime >= #{minUpdateTime}")
+    List<Question> listQuestionWithDelete(Date minUpdateTime);
 
 }
 
