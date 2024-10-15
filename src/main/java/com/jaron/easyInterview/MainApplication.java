43f02ccf25a19b0dd902a6aaadde276384f4804b
jaron <13617035236@163.com>
2024-10-15 10:00:19 +0800

    引入 nacos 实现动态 IP 黑名单过滤

--- src/main/java/com/jaron/easyInterview/MainApplication.java
+++ src/main/java/com/jaron/easyInterview/MainApplication.java
@@ -3,7 +3,7 @@ package com.jaron.easyInterview;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("com.jaron.easyInterview.mapper")
 @EnableScheduling
+@ServletComponentScan
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
 
